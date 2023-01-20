075b89c82521d3ee6007be2e3c9d19740d2e899d
Wolganblack <ALFONSO@DESKTOP-7VNR7GU>
2023-01-20 17:09:30 -0600

    error con nodemon

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+// settings
+app.set('port', process.env.PORT || 4000);
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port 4000');
 });
