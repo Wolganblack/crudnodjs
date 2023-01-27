d7aeb4de8b1bd73757bcb71bad084717a8af2721
Wolganblack <ALFONSO@DESKTOP-7VNR7GU>
2023-01-27 16:44:26 -0600

    prueba 8

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 // settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 
 
 app.listen(app.get('port'), () => {
-    console.log('Server on port 4000');
+    console.log('Server on port 3000');
 });
