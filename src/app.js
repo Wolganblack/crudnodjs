ea4f3523369d612cbf6bdef28e731873fcd7d1e2
Wolganblack <ALFONSO@DESKTOP-7VNR7GU>
2023-02-13 16:04:15 -0600

    ultimo actualizacion

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const mysql = require('mysql2');
+const myConnection = require('express-myconnection');
 // settings
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 
+app.use(myConnection(mysql, {
+    host: 'Localhost',
+    user: 'root',
+    password: 'LINUX1983',
+    port: 3306,
+    database: 'crudnodejsmysql'
+}, 'single'));
+
 
 app.listen(app.get('port'), () => {
     console.log('Server on port 3000');
 });
+
+app.get("/index", (req, res) => {
+
+    res.json({
+        "status": 200,
+        "message": "success"
+    })
+})
